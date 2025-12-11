565a5d79b37c1feaafcdb13bebc9e62e8715c08c
heavycircle <bleeper_genius0c@icloud.com>
2025-12-11 00:37:32 -0500

    2025/11

---
+++ 2025/day-11/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import functools as ft
+import sys
+
+# networkx was super tempting today until it died on part 2.
+# The graph has 596 nodes and 1696 edges. We need a caching
+# solution to get this done.
+
+G = {k[:-1]: v for k, *v in map(str.split, iter(sys.stdin.readline, ""))}
+
+
+@ft.cache
+def count(node, dac=False, fft=False):
+    match node:
+        case "out":
+            return dac and fft
+        case "dac":
+            dac = True
+        case "fft":
+            fft = True
+
+    return sum(count(n, dac, fft) for n in G[node])
+
+
+print(f"ONE: {count('you', True, True)}")
+print(f"TWO: {count('svr', False, False)}")
